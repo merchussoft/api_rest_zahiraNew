e996746d2eaad95c61483cb67f957d3db7b7c28b
damerchan4 <damerchan4@poligran.edu.co>
2020-05-11 22:34:56 -0500

    se agrega el session_start(); al archivo index.php para que las sessiones se activen

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ $app = new \Slim\App($settings);
 $container = $app->getContainer();
 
 require SYSTEMS . 'config/middleware.php';
+session_start();
 require ROUTERS . 'Principal.php';
 //require __DIR__ . '/../src/router/AdminHoras.php';
 //require __DIR__ . '/../src/rutas/login.php';
